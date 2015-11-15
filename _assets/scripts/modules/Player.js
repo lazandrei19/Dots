2802fe5c900ba28a952c0908867dcf1efed3ac62
Lazarescu Andrei <andrei_spion@yahoo.com>
2015-11-15 12:33:26 +0200

    Implemented colors

--- _assets/scripts/modules/Player.js
+++ _assets/scripts/modules/Player.js
@@ -1,17 +1,27 @@
+var colorUtils = require('./colorUtils');
+
 module.exports = (function(){
 	var maxPlayers;
-	var currentPlayer = 0;
 
 
-	function Player (max) {
+	function Player(max) {
 		this.maxPlayers = max - 1;
+		this.currentPlayer = 0;
+		this.colors = [];
+		for(var i = 0; i <= this.maxPlayers;) {
+			var c = colorUtils.randomColor();
+			if(colorUtils.compareDeltas(c, this.colors, 10)) {
+				this.colors.push(c);
+				i++;
+			}
+		}
 	}
 
 	Player.prototype.nextPlayer = function () {
-		if(this.currentPlayer = this.maxPlayers) {
+		if(this.currentPlayer === this.maxPlayers) {
 			this.currentPlayer = 0;
 		} else {
-			this.currentPlayer++;
+			this.currentPlayer += 1;
 		}
 	}
 
@@ -20,7 +30,15 @@ module.exports = (function(){
 	}
 
 	Player.prototype.getMaxPlayers = function () {
-		return ++this.maxPlayers;
+		return this.maxPlayers + 1;
+	}
+
+	Player.prototype.getCurrentColor = function () {
+		return this.colors[this.currentPlayer];
+	}
+
+	Player.prototype.getAllColors = function () {
+		return this.colors;
 	}
 
 	return Player;
