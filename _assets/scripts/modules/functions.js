4f176186b321fddbf101c0b5ff69607ae8d0eb7c
Lazarescu Andrei <andrei_spion@yahoo.com>
2015-11-15 10:49:03 +0200

    Fixed a bug to help with click detection

--- _assets/scripts/modules/functions.js
+++ _assets/scripts/modules/functions.js
@@ -1,7 +1,8 @@
 module.exports.inRange = function (n, step, range, max) {
-	for (var i = 0; i*step < max; i++) {
-		if(n >= i*step - range && n <= i*step + range)
+	for (var i = 0; i*step <= max; i++) {
+		if(n >= i*step - range && n <= i*step + range) {
 			return i;
+		}
 	};
 	return false;
 }
