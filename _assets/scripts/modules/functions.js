17256c39cf1c18ec5cc2ab56a5240ea0abda75be
Lazarescu Andrei <andrei_spion@yahoo.com>
2015-11-15 10:07:47 +0200

    Misc functions file added

---
+++ _assets/scripts/modules/functions.js
@@ -0,0 +1,7 @@
+module.exports.inRange = function (n, step, range, max) {
+	for (var i = 0; i*step < max; i++) {
+		if(n >= i*step - range && n <= i*step + range)
+			return i;
+	};
+	return false;
+}
