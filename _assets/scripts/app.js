76f44a6708c3dc2f05b704ce57196bff6a8597b0
Lazarescu Andrei <andrei_spion@yahoo.com>
2015-11-15 10:48:19 +0200

    Added the config to opts

--- _assets/scripts/app.js
+++ _assets/scripts/app.js
@@ -1,4 +1,4 @@
-var opts = {hDots: 4, vDots: 4};
 var config = require('./modules/config');
+var opts = {hDots: 4, vDots: 4, config};
 var init = require('./modules/init')(opts);
 var engine = require('./modules/dots')(opts);
