b28730a3a28fad56b4e88fbd3d345e70441c0da3
Lazarescu Andrei <andrei_spion@yahoo.com>
2015-11-15 11:17:07 +0200

    Updated to fit  the Player model

--- _assets/scripts/app.js
+++ _assets/scripts/app.js
@@ -1,4 +1,4 @@
 var config = require('./modules/config');
-var opts = {hDots: 4, vDots: 4, config};
+var opts = {hDots: 4, vDots: 4, config, players: 2};
 var init = require('./modules/init')(opts);
 var engine = require('./modules/dots')(opts);
